35d746531ca084185db32439c592e3b1f2ebf0d1
Patrick <pcallahan3@mail.greenriver.edu>
2017-09-11 10:24:02 -0700

    Intergrated the add functionality into the Pages controller so subjects can be displayed with created pages

--- application/controllers/admin/Pages.php
+++ application/controllers/admin/Pages.php
@@ -3,26 +3,36 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Pages extends CI_Controller {
 
-	public function index()
-	{
+	public function index(){
 		//Load template
 		$this->template->load('admin', 'default', 'pages/index');
 	}
 
-	public function add()
-	{
+	public function add(){
+
+
+		$subject_options = array();
+		$subject_options[0] = 'Select Subject';
+
+		$subject_list = $this->Subject_model->get_list();
+
+		foreach($subject_list as $subject){
+			$subject_options[$subject->id] = $subject->name;
+		}
+
+		$data['subject_list'] = $subject_options;
+
+
 		//Load template
-		$this->template->load('admin', 'default', 'pages/add');
+		$this->template->load('admin', 'default', 'pages/add', $data);
 	}
 
-	public function edit()
-	{
+	public function edit(){
 		//Load template
 		$this->template->load('admin', 'default', 'pages/edit');
 	}
 
-	public function delete()
-	{
+	public function delete(){
 		
 	}
 }
--- application/views/admin/pages/add.php
+++ application/views/admin/pages/add.php
@@ -1,1 +1,67 @@
-<h2 class="page-header">Add Page</h2>
+<h2 class="page-header">Add Pages</h2>
+
+<?php echo form_open('admin/pages/add'); ?>
+	<div class="form-group">
+		<?php echo form_label('Page Name', 'name'); ?>
+			<?php $data = array(
+						'name' => 'name',
+						'id' => 'name',
+						'maxlength' => '100',
+						'class' => 'form-control',
+						'value' => set_value('name')
+					);
+			?>
+			<?php echo form_input($data); ?>
+	</div>
+
+	<!--Page subject-->
+	<div class="form-group">
+		<?php echo form_label('Subject', 'subject_id'); ?>
+		<?php echo form_dropdown('subject_id', '$subject_options', 0, array()); ?>
+	</div>
+
+	<!--Page body-->
+	<div class="form-group">
+		<?php echo form_label('Body', 'subject_id');  ?>
+		<?php
+			$data = array(
+					'name'    => 'body',
+					'id'      => 'body',
+					'class'   => 'form-control',
+					'value'   => set_value('subject')
+
+			);
+		?>
+		<?php echo form_textarea($data); ?>
+	</div>
+
+	<!--Publish-->
+	<div class="form-group">
+		<?php echo form_label('Published', 'is_published'); ?>
+		<?php echo form_radio('is_published', 1, TRUE); ?> Yes
+		<?php echo form_radio('is_published', 0,FALSE); ?> No
+	</div>
+
+	<!--Feature-->
+	<div class="form-group">
+		<?php echo form_label('Feature', 'is_featured'); ?>
+		<?php echo form_radio('is_featured', 1, FALSE); ?> Yes
+		<?php echo form_radio('is_featured', 0, TRUE); ?> No
+	</div>
+
+	<!--Publish-->
+	<div class="form-group">
+		<?php echo form_label('Add To Menu', 'in_menu'); ?>
+		<?php echo form_radio('in_menu', 1, TRUE); ?> Yes
+		<?php echo form_radio('in_menu', 0,FALSE); ?> No
+	</div>
+
+	<!--Order-->
+	<div class="form-group">
+		<?php echo form_label('Order', 'order');  ?>
+		<input class="form-control" name="order" id="order" type="number">
+	</div>
+
+	<?php echo form_submit('mysubmit', 'Add Page', array('class' => 'btn btn-primary')); ?>
+
+<?php echo form_close(); ?>
